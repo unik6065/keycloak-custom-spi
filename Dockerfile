03738d3191e7f7e1d958fb0d1976e6363f1fc77f
romain isoz <isozromain@gmail.com>
2024-03-15 23:05:47 +0100

    init commit

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM maven:3.8.4-openjdk-17 AS build
+
+WORKDIR /app
+
+COPY . /app/
+
+RUN mvn clean package
+
+FROM quay.io/keycloak/keycloak:23.0.7
+
+COPY --from=build app/target/keycloak-spi-kafka.jar /opt/keycloak/providers
+
+ENTRYPOINT ["/bin/sh", "-c", "/opt/keycloak/bin/kc.sh start-dev"]
---
+++ io/github/akoserwal/KeycloakCustomEventListener.java
@@ -0,0 +1,29 @@
+package io.github.akoserwal;
+
+import org.keycloak.events.Event;
+import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.admin.AdminEvent;
+
+public class KeycloakCustomEventListener implements EventListenerProvider {
+
+  private static final Logger log = Logger.getLogger(MyListenerProvider.class.getName());
+
+	@Override
+	public void onEvent(Event event) {
+			log.info("Event:-"+event.getUserId());
+      throw new RuntimeException("Intentional exception for testing logging");
+
+	}
+
+	@Override
+	public void onEvent(AdminEvent adminEvent, boolean b) {
+			log.info("Admin Event:-"+adminEvent.getResourceType().name());
+      throw new RuntimeException("Intentional exception for testing logging");
+
+	}
+
+	@Override
+	public void close() {
+
+	}
+}
