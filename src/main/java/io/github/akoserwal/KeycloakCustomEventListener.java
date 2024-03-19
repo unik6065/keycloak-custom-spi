966a71feacb6dcae014da3c2e5d8f7e181dc13f4
romain isoz <isozromain@gmail.com>
2024-03-19 18:03:24 +0100

    change repo layout

--- src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
+++ src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
@@ -6,18 +6,18 @@ import org.keycloak.events.admin.AdminEvent;
 
 public class KeycloakCustomEventListener implements EventListenerProvider {
 
-  private static final Logger log = Logger.getLogger(MyListenerProvider.class.getName());
+  // private static final Logger log = Logger.getLogger(MyListenerProvider.class.getName());
 
 	@Override
 	public void onEvent(Event event) {
-			log.info("Event:-"+event.getUserId());
+			// log.info("Event:-"+event.getUserId());
       throw new RuntimeException("Intentional exception for testing logging");
 
 	}
 
 	@Override
 	public void onEvent(AdminEvent adminEvent, boolean b) {
-			log.info("Admin Event:-"+adminEvent.getResourceType().name());
+			// log.info("Admin Event:-"+adminEvent.getResourceType().name());
       throw new RuntimeException("Intentional exception for testing logging");
 
 	}
