580a8f16154232bb7d530ca49c649ef0b947fcfe
romain isoz <isozromain@gmail.com>
2024-03-20 21:51:26 +0100

    seems to work, IDs not matching when creating a user from admin

--- src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
+++ src/main/java/io/github/akoserwal/KeycloakCustomEventListener.java
@@ -1,24 +1,34 @@
 package io.github.akoserwal;
 
 import org.keycloak.events.Event;
+import org.keycloak.events.EventType;
 import org.keycloak.events.EventListenerProvider;
+import org.keycloak.events.admin.OperationType;
 import org.keycloak.events.admin.AdminEvent;
 
 public class KeycloakCustomEventListener implements EventListenerProvider {
 
-  // private static final Logger log = Logger.getLogger(MyListenerProvider.class.getName());
-
 	@Override
 	public void onEvent(Event event) {
-			// log.info("Event:-"+event.getUserId());
-      throw new RuntimeException("Intentional exception for testing logging");
+      if (EventType.REGISTER.equals(event.getType()) ) {
+        System.out.println("User register as:-"+event.getUserId());
+      }
+
+      if (EventType.DELETE_ACCOUNT.equals(event.getType()) ) {
+        System.out.println("User delete as:-"+event.getUserId());
+      }
 
 	}
 
 	@Override
 	public void onEvent(AdminEvent adminEvent, boolean b) {
-			// log.info("Admin Event:-"+adminEvent.getResourceType().name());
-      throw new RuntimeException("Intentional exception for testing logging");
+    if (OperationType.CREATE.equals(adminEvent.getOperationType()) ) {
+      System.out.println("User register from admin:-"+adminEvent.getAuthDetails().getUserId());
+    }
+
+    if(OperationType.DELETE.equals(adminEvent.getOperationType()) ) {
+      System.out.println("User delete from admin:-"+adminEvent.getAuthDetails().getUserId());
+    }
 
 	}
 
